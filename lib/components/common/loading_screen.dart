47c3166b74f808fc60b12174c169d6b93f08c982
wonhj12 <wonhj12@naver.com>
2025-03-15 10:28:12 +0900

    feat: 로딩 컴포넌트 구현

---
+++ lib/components/common/loading_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tickle/utils/palette.dart';
+
+class LoadingScreen extends StatelessWidget {
+  final double? value;
+  const LoadingScreen({super.key, this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const ModalBarrier(dismissible: false),
+        Center(
+          child: CircularProgressIndicator(
+            color: Palette.primary,
+            value: value,
+          ),
+        ),
+      ],
+    );
+  }
+}
